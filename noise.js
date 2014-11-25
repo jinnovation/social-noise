af41fc18638bf62ae27cec5f5509bbc714b0b455
Jonathan Jin <jjin082693@gmail.com>
2014-11-24 19:59:49 -0600

    randomize hashtag location + general buildup

--- noise.js
+++ noise.js
@@ -1,38 +1,47 @@
 "use strict";
 
 (function () {
-
-  var hashtagsFile = "hashtags.txt";
+  var hashtagsFile       = "hashtags.txt"; // TODO: pull from Twitter API 
 
   var entryPointSelector = "body";
 
-  var margin    = {top: 30, right: 20, bottom: 30, left: 20};
-  var width     = 960 - margin.left - margin.right;
+  var margin             = { top: 30, right: 20, bottom: 30, left: 20};
+  var width              = 960 - margin.left - margin.right;
+  var height = 480 - margin.top - margin.bottom;
+
+  var hashtagData;
+  var hashtagText;
 
   var svg = d3.select(entryPointSelector)
     .append("svg")
     .attr("width", width)
-    .attr("height", "1000");
+    .attr("height", height)
+    .append("g")
+    .attr("id", "hashtags")
+    .attr("class", "container")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var dsv = d3.dsv("\n", "text/plain");
 
-  var hashtags;
   dsv(hashtagsFile, function (error, data) {
-    hashtags = data;
+    hashtagData = data;
 
     update();
   })
 
   function update() {
-    svg.selectAll("text")
-      .data(hashtags)
+    hashtagText = svg.selectAll("text")
+      .data(hashtagData)
       .enter()
       .append("text")
-      .attr("x", margin.left)
-      .attr("y", margin.top)
+      .attr("class", "hashtag")
       .text(function (d) { return d.tag; })
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "20px")
-      .attr("fill", "black");
+      .attr("x", function (d) { return Math.random() * width; })
+      .attr("y", function (d) { return Math.random() * height;});
+
+    hashtagText
+      .attr("font-family", "sans-serif") // TODO: set font elsewhere
+      .attr("font-size", "20px") // TODO: randomize 
+      .attr("fill", "black"); // TODO: randomize 
   }
 })();
