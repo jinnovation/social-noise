911a9ee6c79bb20c63dd6c16688c11cd220b0224
Jonathan Jin <jjin082693@gmail.com>
2014-11-24 21:00:17 -0600

    randomize font size and color scheme

--- noise.js
+++ noise.js
@@ -1,13 +1,31 @@
 "use strict";
 
+var colorScheme = {
+  bg: d3.rgb(14,255,0),
+  text: [
+    d3.rgb(255,238,0),
+    d3.rgb(238,51,34),
+    d3.rgb(0,119,238)
+  ]
+};
+
+var fontSize = {
+  min: 20,
+  max: 40
+};
+
+function randRange(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
+
 (function () {
-  var hashtagsFile       = "hashtags.txt"; // TODO: pull from Twitter API 
+  var hashtagsFile       = "hashtags.txt"; // TODO: pull from Twitter API
 
   var entryPointSelector = "body";
 
-  var margin             = { top: 30, right: 20, bottom: 30, left: 20};
+  var margin             = { top: 30, right: 20, bottom: 30, left: 20 };
   var width              = 960 - margin.left - margin.right;
-  var height = 480 - margin.top - margin.bottom;
+  var height             = 480 - margin.top - margin.bottom;
 
   var hashtagData;
   var hashtagText;
@@ -16,6 +34,7 @@
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .attr("style", "background-color: " + colorScheme.bg + ";")
     .append("g")
     .attr("id", "hashtags")
     .attr("class", "container")
@@ -41,7 +60,12 @@
 
     hashtagText
       .attr("font-family", "sans-serif") // TODO: set font elsewhere
-      .attr("font-size", "20px") // TODO: randomize 
-      .attr("fill", "black"); // TODO: randomize 
+      .attr("font-size", function () {
+        return randRange(fontSize.min, fontSize.max);
+      })
+      .attr("fill", function () {
+        var randIndex = Math.floor(Math.random() * colorScheme.text.length);
+        return colorScheme.text[randIndex];
+      });
   }
 })();
